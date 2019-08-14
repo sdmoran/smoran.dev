0fd346fdc0a4d912abc9fcb1e9c9e32e9fb8cb2e
sdmoran <sdhm87@gmail.com>
2019-08-13 17:36:02 -0700

    oops forgot to comment out

--- fresh/settings.py
+++ fresh/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-file = open('.\\fresh\\secret_key.txt')
-data = file.read().strip()
-file.close()
+# file = open('.\\fresh\\secret_key.txt')
+# data = file.read().strip()
+# file.close()
 SECRET_KEY = "test"
 
 # SECURITY WARNING: don't run with debug turned on in production!
