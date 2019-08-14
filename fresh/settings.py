2531d1d8a5d8aa5d26c89892e8208a90b9e6133f
sdmoran <sdhm87@gmail.com>
2019-08-13 17:27:01 -0700

    Added/modified files for deployment on Heroku

--- fresh/settings.py
+++ fresh/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +126,9 @@ X_FRAME_OPTIONS = 'DENY'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/templates/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
