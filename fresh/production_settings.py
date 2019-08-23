6d8424409036f0ffc08a92453baa94a66683e0a9
sdmoran <sdhm87@gmail.com>
2019-08-23 19:56:18 +0000

    Slightly changed way secret key is set - no default now

--- fresh/production_settings.py
+++ fresh/production_settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # Sets secret key depending on OS environment variable
-SECRET_KEY = os.environ.get('SECRET_KEY', 'a-really-bad-secret-key')
+SECRET_KEY = str(os.environ['SECRET_KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
